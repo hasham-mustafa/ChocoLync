ae70566e0ad3dc49e9dbf47c3312ba335b552a23
Hasham Mustafa <hashamkhattak024@gmail.com>
2026-07-14 15:45:41 +0500

    Role Authorization

---
+++ RoleAuthorizeAttribute.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ChocoLync.Filters
+{
+    public class RoleAuthorizeAttribute : ActionFilterAttribute
+    {
+        private readonly string[] _roles;
+
+        public RoleAuthorizeAttribute(string roles)
+        {
+            _roles = roles.Split(',', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        public override void OnActionExecuting(
+            ActionExecutingContext context)
+        {
+            var role =
+                context.HttpContext.Session
+                .GetString("Role");
+
+            if (!_roles.Contains(role))
+            {
+                context.Result =
+                    new RedirectToActionResult(
+                        "AccessDenied",
+                        "Account",
+                        null);
+            }
+        }
+    }
+}
